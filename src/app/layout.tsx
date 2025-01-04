b434a5fb71880168a08ea08e6a137f43dc2c1ac6
Susanto <sushantomahato1@gmail.com>
2025-01-04 14:03:01 +0530

    initial setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { DM_Sans } from "next/font/google";
+import { ThemeProvider } from "@/providers/theme-provider";
+import "./globals.css";
+
+const font = DM_Sans({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "BridgeX",
+  description: "Automate your work with BridgeX.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Navbar from "@/components/global/navbar";
+
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center flex-col">
+      <Navbar />
+    </main>
+  );
+}
